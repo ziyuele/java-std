de04fe3af632e4d06397e61db0cf806622659ca6
ziyuele <2429362606@qq.com>
2022-07-24 21:57:58 +0800

    test then appley / then appleyasync

--- java-study/src/test/java/com/kangjian/tmptestmt/Test202103.java
+++ java-study/src/test/java/com/kangjian/tmptestmt/Test202103.java
@@ -3,6 +3,8 @@ package com.kangjian.tmptestmt;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.concurrent.CompletableFuture;
+
 @Slf4j
 public class Test202103 {
 
@@ -11,5 +13,27 @@ public class Test202103 {
     public void testStringFormat() {
         double num = 3d;
         System.out.println(String.format("%.1f", num));
+    }
+
+    @Test
+    public void testThenApplay() {
+        long time = System.currentTimeMillis();
+        CompletableFuture<Integer> completableFuture = CompletableFuture.completedFuture(1);
+
+        completableFuture.thenApplyAsync( t -> {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+            return 1;
+        });
+        completableFuture.thenApplyAsync( t -> {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+            return 1;
+        });
+        System.out.println(System.currentTimeMillis() - time);
     }
 }
